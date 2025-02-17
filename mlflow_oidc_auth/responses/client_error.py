1275657c96aff2d7a4269a295b1066b2e7d86e8d
Alexander Kharkevich <alex@kharkevich.org>
2025-02-17 15:33:43 -0500

    chore: update references

--- mlflow_oidc_auth/responses/client_error.py
+++ mlflow_oidc_auth/responses/client_error.py
@@ -15,7 +15,7 @@ def make_forbidden_response() -> Response:
 
 def make_basic_auth_response() -> Response:
     response = make_response(
-        "You are not authenticated. Please see documentation for details" "https://github.com/data-platform-hq/mlflow-oidc-auth"
+        "You are not authenticated. Please see documentation for details" "https://github.com/mlflow-oidc/mlflow-oidc-auth"
     )
     response.status_code = 401
     response.headers["WWW-Authenticate"] = 'Basic realm="mlflow"'
