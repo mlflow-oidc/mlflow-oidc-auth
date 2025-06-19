a542cadfebb53571fbe5147177281556e748a552
yao <yao@REVMBP143.local>
2025-06-19 11:44:20 +0200

    fix(Mlflow-oidc-auth): Resolve issue when user is in lot of group. Resolve issue when user in EntraID with tech account from Onpremise AD
    email is also retrieve from  "prefered_username" when email field is not settup from TechAccount in AD and add '@odata.nextLink' to get all groups for user

--- mlflow_oidc_auth/plugins/group_detection_microsoft_entra_id/__init__.py
+++ mlflow_oidc_auth/plugins/group_detection_microsoft_entra_id/__init__.py
@@ -1,14 +1,28 @@
 import requests
-
+from mlflow_oidc_auth.config import config
 
 def get_user_groups(access_token):
-    graph_url = "https://graph.microsoft.com/v1.0/me/memberOf"
-    group_response = requests.get(
-        graph_url,
-        headers={
-            "Authorization": f"Bearer {access_token}",
-            "Content-Type": "application/json",
-        },
-    )
-    group_data = group_response.json()
+    graph_url="https://graph.microsoft.com/v1.0/me/memberOf"
+    headers={
+        "Authorization":f"Bearer{access_token}",
+        "Content-Type":"application/json",
+    }
+
+    # Initialize an empty list to hold all groups
+    all_groups=[]
+
+    while graph_url:
+        # Make the request to get the user's groups
+        group_response=requests.get(graph_url,headers=headers)
+
+        # Check if the response is successful
+        if not group_response.ok:
+            raise Exception(f"Error retrieving user groups: {group_response.status_code}-{group_response.text}")
+
+        # Parse the response JSON
+        group_data=group_response.json()
+
+        # Check for more pages of results
+        graph_url=group_data.get('@odata.nextLink')
+
     return list(dict.fromkeys([group["displayName"] for group in group_data["value"] if group.get("displayName") is not None]))
