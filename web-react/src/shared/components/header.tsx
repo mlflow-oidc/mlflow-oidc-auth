bf5a94cb75dadd6b9de3570c55cbbf5279f8db12
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-10-09 10:31:26 -0400

    feat: create header

---
+++ web-react/src/shared/components/header.tsx
@@ -0,0 +1,17 @@
+import DarkModeToggle from "./dark-mode-toggle";
+
+const Header = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white p-8">
+        <DarkModeToggle />
+        <h1 className="mt-8 text-3xl font-bold">Dark Mode Test</h1>
+        <p className="mt-4">
+          This is some sample text to see the light/dark mode toggle in action.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
