bd14010872c56dd2c93c371f3f3f8f40e3fba997
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-10-09 22:42:51 -0400

    feat: add  route wrappers (auth)

--- web-react/src/app.tsx
+++ web-react/src/app.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router";
+import ProtectedRoute from "./features/auth/protected-route";
+import RedirectIfAuth from "./features/auth/redirect-if-auth";
 
 const AuthPage = React.lazy(() => import("./features/auth/auth-page"));
 const UserPage = React.lazy(() => import("./features/user/user-page"));
@@ -8,10 +10,34 @@ const ManagePage = React.lazy(() => import("./features/manage/manage-page"));
 export default function App() {
   return (
     <Routes>
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/user" element={<UserPage />} />
-      <Route path="/manage" element={<ManagePage />} />
+      <Route
+        path="/auth"
+        element={
+          <RedirectIfAuth fallback={<div>Loading...</div>}>
+            <AuthPage />
+          </RedirectIfAuth>
+        }
+      />
 
+      <Route
+        path="/user/*"
+        element={
+          <ProtectedRoute fallback={<div>Loading...</div>}>
+            <UserPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/manage/*"
+        element={
+          <ProtectedRoute fallback={<div>Loading...</div>}>
+            <ManagePage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="/403" element={<div>Forbidden</div>} />
       <Route path="*" element={<Navigate to="/user" replace />} />
     </Routes>
   );
