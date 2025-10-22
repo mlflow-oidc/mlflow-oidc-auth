832dc08087f9c26b1da6eabeff6d0824ec6aae72
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-10-22 00:44:43 -0400

    feat: update user page

--- web-react/src/features/user/user-page.tsx
+++ web-react/src/features/user/user-page.tsx
@@ -1,9 +1,30 @@
+import Header from "../../shared/components/header";
+
 export const UserPage = () => {
+  // Placeholder for the logged-in user's name
+  const userName = "Alice";
+
   return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold mb-4">User Page</h1>
-      <p>This is a placeholder for the User page.</p>
-    </div>
+    <>
+      <Header userName={userName} />
+      <div
+        className="
+          p-8 shadow-md min-h-screen
+          bg-[rgb(255,255,255)] text-[rgb(17,23,28)]
+          dark:bg-[rgb(17,23,28)] dark:text-[rgb(232,236,240)]
+        "
+      >
+        <h1 className="mt-8 text-3xl font-bold">Dark Mode Test</h1>
+        <p className="mt-4">
+          This is some sample text to see the light/dark mode toggle in action.
+        </p>
+
+        <div className="p-6 text-center mt-12">
+          <h2 className="text-2xl font-bold mb-4">User Page</h2>
+          <p>This is a placeholder for the User page.</p>
+        </div>
+      </div>
+    </>
   );
 };
 
