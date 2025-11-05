d488287f7a1d1cc75cf83637762f4bb9a152fc69
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-11-05 00:02:08 -0500

    feat: update AppLink component

--- web-react/src/shared/components/header-desktop-nav.tsx
+++ web-react/src/shared/components/header-desktop-nav.tsx
@@ -1,33 +1,37 @@
 import React from "react";
-import NavLink from "./nav-link";
 import { type NavigationData } from "./navigation-data";
+import AppLink from "./app-link";
 
 const HeaderDesktopNav: React.FC<NavigationData> = ({
   mainLinks,
   userControls,
 }) => {
+  const linkClasses =
+    "p-2 w-full sm:w-auto text-text-primary hover:text-text-primary-hover dark:text-text-primary-dark dark:hover:text-text-primary-hover-dark text-lg sm:text-sm font-medium transition-colors text-left sm:text-center rounded-md";
   return (
     <div className="hidden sm:flex justify-between items-center">
       <nav className="flex flex-col sm:flex-row space-y-2 sm:space-y-0">
         {mainLinks.map((link) => (
-          <NavLink
+          <AppLink
             key={link.label}
             href={link.href}
             isInternalLink={link.isInternalLink}
+            className={linkClasses}
           >
             {link.label}
-          </NavLink>
+          </AppLink>
         ))}
       </nav>
       <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0">
         {userControls.map((link) => (
-          <NavLink
+          <AppLink
             key={link.label}
             href={link.href}
             isInternalLink={link.isInternalLink}
+            className={linkClasses}
           >
             {link.label}
-          </NavLink>
+          </AppLink>
         ))}
       </div>
     </div>
