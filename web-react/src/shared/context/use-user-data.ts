c0ee323460c38adefd4638d0f40f0c989896ad27
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-11-07 12:40:16 -0500

    feat: update fetching user data

--- web-react/src/shared/context/use-user-data.ts
+++ web-react/src/shared/context/use-user-data.ts
@@ -1,12 +1,4 @@
 import { use } from "react";
 import { UserContext } from "../context/user-provider";
-import type { UserContextType } from "../types/user";
 
-export const useUserData = (): UserContextType => {
-  const context = use(UserContext);
-
-  if (context === undefined) {
-    throw new Error("useUserData must be used within a UserProvider");
-  }
-  return context;
-};
+export const useUserData = () => use(UserContext);
