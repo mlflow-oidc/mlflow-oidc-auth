5efce20536b08aae14fa95f5563e5d1c2b3dc28d
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-11-19 11:00:40 -0500

    feat: update 403 page

--- web-react/src/features/forbidden/forbidden-page.tsx
+++ web-react/src/features/forbidden/forbidden-page.tsx
@@ -3,7 +3,14 @@ import MainLayout from "../../shared/components/main-layout";
 export default function ForbiddenPage() {
   return (
     <MainLayout>
-      <h2 className="text-3xl font-semibold mb-6 text-center">Forbidden</h2>
+      <div className="flex flex-col h-full items-center justify-center px-4">
+        <h2 className="text-4xl sm:text-5xl font-extrabold mb-4 text-logo">
+          Access Denied
+        </h2>
+        <p className="text-lg text-center max-w-lg mb-10 text-text-primary dark:text-text-primary-dark">
+          Sorry, you do not have permission to view this page.
+        </p>
+      </div>
     </MainLayout>
   );
 }
