50c3e39cd4af0e80aa9105ae6fad3bbb58852ea4
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-11-28 13:53:24 -0500

    feat: add fetching all users, update users page

--- web-react/src/core/services/user-service.ts
+++ web-react/src/core/services/user-service.ts
@@ -15,3 +15,13 @@ export async function fetchCurrentUser(
     signal,
   });
 }
+
+export async function fetchAllUsers(signal?: AbortSignal): Promise<string[]> {
+  const cfg = await getRuntimeConfig(signal);
+  const allUsersUrl = `${cfg.basePath}${API_ENDPOINTS.ALL_USERS}`;
+
+  return http<string[]>(allUsersUrl, {
+    method: "GET",
+    signal,
+  });
+}
