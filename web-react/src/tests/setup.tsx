17312761e12dfe1cfcf88c7add73bd50f8d47c4c
Olga Kharkevich <olga.kharkevich@gmail.com>
2026-01-27 15:54:36 -0500

    refactor: replace Modal implementation with <dialog>

--- web-react/src/tests/setup.tsx
+++ web-react/src/tests/setup.tsx
@@ -1,1 +1,19 @@
 import "@testing-library/jest-dom";
+import { vi, beforeAll } from "vitest";
+
+// JSDOM doesn't support <dialog> yet
+beforeAll(() => {
+  if (typeof window !== "undefined") {
+    HTMLDialogElement.prototype.showModal = vi.fn(function (
+      this: HTMLDialogElement,
+    ) {
+      this.open = true;
+    });
+    HTMLDialogElement.prototype.close = vi.fn(function (
+      this: HTMLDialogElement,
+    ) {
+      this.open = false;
+    });
+  }
+});
+
